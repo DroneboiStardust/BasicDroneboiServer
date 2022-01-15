4aa45ec7dbf6148e1758bd77c08f8380838e420f
Stardust <89341766+asaex10@users.noreply.github.com>
2022-01-15 02:36:53 +0000

    Changes

--- Error.cs
+++ Error.cs
@@ -6,7 +6,9 @@ namespace Error
     {
         public SoftError(string str)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Error: "+str);
+            Console.ResetColor();
             this.errorStr = str;
         }
         public string errorStr;
--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using Error;
 using UnityEngine;
 using PacketClass;
+
+using TcpChatServer;
 
 class Program
 {
@@ -28,5 +33,50 @@ class Program
         Console.WriteLine(vector3.x+","+vector3.y+","+vector3.z);
 
         Console.WriteLine("id:"+packet.GetId());
+
+            
+            
+            // TCP server port
+            int port = 3258;
+
+            Console.WriteLine($"TCP server port: {port}");
+
+            Console.WriteLine();
+
+            // Create a new TCP chat server
+            var server = new ChatServer(IPAddress.Any, port);
+
+            // Start the server
+            Console.Write("Server starting...");
+            server.Start();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
+
+            // Perform text input
+            for (;;)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // Restart the server
+                if (line == "!")
+                {
+                    Console.Write("Server restarting...");
+                    server.Restart();
+                    Console.WriteLine("Done!");
+                    continue;
+                }
+
+                // Multicast admin message to all sessions
+                line = "(admin) " + line;
+                server.Multicast(line);
+            }
+
+            // Stop the server
+            Console.Write("Server stopping...");
+            server.Stop();
+            Console.WriteLine("Done!");
     }
 }
